4340d913ed9f2743305aec661ff96a87003916c2
jokessenich <johnny.kessenich@gmail.com>
2019-12-20 13:50:57 -0800

    small server changes

--- src/app.js
+++ src/app.js
@@ -18,6 +18,16 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
 app.get('/', (req, res)=> {
     res.send('Hello, world!')
 })
--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8001,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL:"postgresql://dunder_mifflin:a@localhost/our_health" 
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://dunder_mifflin:a@localhost/our_health" ,
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://dunder_mifflin:a@localhost/our_health-test"
+
+    
   }
